3c5f0549ed0f417b7998ebdf61ee3c0d0e937447
Nikita Pavets <nikitapavets@gmail.com>
2024-02-19 18:26:14 +0300

    move logger

--- example/app.ts
+++ example/app.ts
@@ -1,11 +1,9 @@
-import { LoggerModule } from '@iamnnort/nestjs-logger';
 import { Module } from '@nestjs/common';
 import { RequestModule } from '../src';
 import { DemoCommand } from './command';
 
 @Module({
   imports: [
-    LoggerModule,
     RequestModule.forFeature({
       name: 'Demo Api',
       baseUrl: 'https://jsonplaceholder.typicode.com',
--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { LoggerModule } from '@iamnnort/nestjs-logger';
 import { ConfigurableModuleClass } from './module-definition';
 import { RequestService } from './service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, LoggerModule],
   providers: [RequestService],
   exports: [RequestService],
 })
