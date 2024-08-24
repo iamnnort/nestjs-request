53318cf711db784fb8889c3abe407a8680008378
Nikita Pavets <nikitapavets@gmail.com>
2024-08-24 15:59:45 +0300

    add timeout

--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,7 @@ export type RequestConfig = Omit<AxiosRequestConfig, 'baseURL' | 'url'> & {
   xml?: boolean;
 };
 
-export type BaseRequestConfig = Pick<AxiosRequestConfig, 'auth' | 'headers'> & {
+export type BaseRequestConfig = Pick<AxiosRequestConfig, 'auth' | 'headers' | 'timeout'> & {
   name?: string;
   baseUrl?: string;
   baseUrlName?: string;
